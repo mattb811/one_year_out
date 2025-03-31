013c8fd974e345abbc7434971145d514cc6bc47a
mattb811 <mattbailey811@gmail.com>
2025-03-31 20:03:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+document.title = 'Guess the Year 🎶';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
